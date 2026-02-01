e0009fa8d0e77e07d9727e31693341a301f2c098
dpambala <dpambala@workforcesoftware.com>
2026-02-01 21:54:18 +0530

    Remove embeddings from repo and ignore vector DB

--- llm_factory.py
+++ llm_factory.py
@@ -3,7 +3,7 @@ import os
 
 def get_llm():
     return ChatGoogleGenerativeAI(
-        model="gemini-2.5-flash",
+        model="gemini-2.0-flash",
         temperature=0,
         max_retries=2
     )
